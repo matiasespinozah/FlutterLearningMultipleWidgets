3768b8551f9387321b038030f99b91ee6553f296
Matías Espinoza <matias@espinozah.cl>
2023-07-26 14:20:46 -0400

    go router configurado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
@@ -48,9 +49,7 @@ class _CustomListTile extends StatelessWidget {
       trailing: const Icon(Icons.arrow_forward_ios_outlined),
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subtitle),
-      onTap: () {
-        Navigator.pushNamed(context, menuItem.link);
-      },
+      onTap: () => context.push(menuItem.link),
     );
   }
 }
